b564393d68097730a43bf924af25e510e5e545b1
samzah <szahir@hotmail.com>
2016-03-03 17:14:25 -0500

    we almost got it

--- js/main.js
+++ js/main.js
@@ -5,9 +5,22 @@ var adj = prompt('An adjective (beautiful, super, etc.)');
 var past = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning');
 
-var writeAstory = function (colour, creature, adj, past) {
-  document.write('After snacking on' + adj + ' treats, the ' + colour + 'bellied' + creature + past + 'for hours');
+var writeAstoryA = function (colour, creature, adj, past) {
+  document.write('After snacking on ' + adj + ' treats, the ' + colour + ' bellied ' + creature + ' ' + past + ' for hours ');
 };
+
+var writeAstoryB = function (colour, creature, adj, past) {
+  document.write('slurping on an ass ' + adj + ' treats, the ' + colour + ' bellied ' + creature + ' ' + past + ' for hours ');
+};
+
+var writeAstoryC = function (colour, creature, adj, past) {
+  document.write('shitting in a box ' + adj + ' treats, the ' + colour + ' bellied ' + creature + ' ' + past + ' for hours ');
+};
+
+
+writeAstoryA(colour, creature, adj, past);
+writeAstoryB(colour, creature, adj, past);
+writeAstoryC(colour, creature, adj, past);
 
 
 
